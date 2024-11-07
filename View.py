bf74fdf926b9e0b2a8bc81ef3fb305ea042fa178
mandosanti06 <armando.santiago4@upr.edu>
2024-11-07 19:32:08 -0400

    Ballot Test for Write In and View.py upgrades

--- View.py
+++ View.py
@@ -15,19 +15,19 @@ adaptive_thresh = cv2.adaptiveThreshold(
 
 # Define coordinates for checkboxes
 checkboxes = {
-    "Partido Nuevo Progresista": (496, 333, 115, 85),
-    "Partido Popular Democrático": (1057, 333, 115, 85),
-    "Movimiento Victoria Ciudadana": (1618, 333, 115, 85),
-    "Partido Independentista Puertorriqueño": (2179, 333, 115, 85),
-    "Proyecto Dignidad": (2740, 333, 115, 85),
+    "Partido Nuevo Progresista": (497, 334, 114, 84),
+    "Partido Popular Democrático": (1058, 334, 114, 84),
+    "Movimiento Victoria Ciudadana": (1619, 334, 114, 84),
+    "Partido Independentista Puertorriqueño": (2180, 334, 114, 84),
+    "Proyecto Dignidad": (2742, 335, 113, 83),
     "Jennifer González": (355, 636, 77, 52),
     "William Villafañe": (355, 817, 77, 52),
     "Jesús Manuel Ortiz": (917, 636, 77, 52),
-    "Pablo José Hernández Rivera": (917, 817, 77, 52),  # Adjusted coordinates
+    "Pablo José Hernández Rivera": (917, 817, 77, 52),
     "Javier Córdova Iturregui": (1479, 636, 76, 52),
     "Ana Irma Rivera Lassén": (1479, 817, 76, 52),
-    "Juan Dalmau": (2041, 636, 76, 51),  # Adjusted coordinates
-    "Roberto Karlo Velázquez Correa": (2041, 817, 76, 51),
+    "Juan Dalmau": (2041, 636, 76, 51),
+    "Roberto Karlo Velázquez Correa": (2040, 818, 75, 50),
     "Javier Jiménez Pérez": (2603, 636, 76, 51),
     "Viviana Ramírez Morales": (2603, 817, 76, 51),
     "Write-in Gobernador": (3165, 636, 76, 51),
